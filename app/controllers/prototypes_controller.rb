cc9a6813996c3a45cf70469a29bc2672789199ac
yutaro910 <yayantsukebo@gmail.com>
2021-03-11 18:40:19 +0900

    editアクションへのリダイレクト設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,5 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :move_to_index, only: :edit
 
   def index
     @prototypes = Prototype.all
@@ -27,6 +26,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless @prototype.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
 
   def update
@@ -47,11 +49,5 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    if user_signed_in? != current_user.id
-      redirect_to action: :index
-    end
   end
 end
